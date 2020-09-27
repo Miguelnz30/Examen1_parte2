5f1c11d8821b1da359f09a21479771d9b7d171b7
Miguelnz30 <miguelcanales72@hotmail.com>
2020-09-27 14:11:23 -0600

    parte2

---
+++ wp-content/plugins/cliengo/public/js/script_install_cliengo.js
@@ -0,0 +1,1 @@
+(function(){var ldk=document.createElement("script"); ldk.type="text/javascript";ldk.async=true; ldk.src="https://s.cliengo.com/weboptimizer/5f70bc3933c16d002a582421/5f70bc3933c16d002a582424.js?platform=wordpress"; var s=document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ldk, s);})();
